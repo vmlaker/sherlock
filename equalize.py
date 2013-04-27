5f84f025d420f5216a9493afd86149ec84d2278e
vmlaker <velimir.mlaker@gmail.com>
2013-04-27 08:58:36 -0700

    Remove obsolete tests.

--- equalize.py
+++
@@ -1,8 +0,0 @@
-import cv2
-cap = cv2.VideoCapture(0)
-while True:
-    f, image = cap.read()
-    image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    image = cv2.equalizeHist(image)
-    cv2.imshow('input', image)
-    cv2.waitKey(1)
--- playback.py
+++
@@ -1,6 +0,0 @@
-import cv2
-cap = cv2.VideoCapture(0)
-while True:
-    f, image = cap.read()
-    cv2.imshow('input', image)
-    cv2.waitKey(1)
