5d06ebb5111d10f0ef52dd752f952569435a6abe
vmlaker <velimir.mlaker@gmail.com>
2014-01-12 09:25:23 -0800

    Add combination cascade file search.

--- src/util/cascade.py
+++ src/util/cascade.py
@@ -3,48 +3,75 @@
 import os 
 import cv2
 
-path_to_haar = '/usr/share/OpenCV/haarcascades'
-path_to_lbp = '/usr/share/OpenCV/lbpcascades'
+# List of directories that are searched for classifier files.
+dirs = (
 
+    # On Fedora.
+    '/usr/share/OpenCV/haarcascades',
+    '/usr/share/OpenCV/lbpcascades',
+
+    # On Debian.
+    '/usr/share/opencv/haarcascades',
+    '/usr/share/opencv/lbpcascades',
+)
+
+# Listed below are classifiers used 
+# (file names sans file extension.)
+# Color values are in B,G,R format.
 specs = (
     # ===== Face =====
-    #('haarcascade_frontalface_alt2.xml'    , path_to_haar, (0, 255, 0)),
-    #('haarcascade_frontalface_alt_tree.xml', path_to_haar, (0, 255, 0)),
-    #('haarcascade_frontalface_alt.xml'     , path_to_haar, (0, 255, 0)),
-    #('haarcascade_frontalface_default.xml' , path_to_haar, (127, 255, 127)),
-    #('haarcascade_profileface.xml'         , path_to_haar, (127, 255, 127)),
-    #('lbpcascade_frontalface.xml'          , path_to_lbp,  (127, 255, 127)),
+    ('haarcascade_frontalface_alt2'    , (0, 255, 0)),
+    #('haarcascade_frontalface_alt_tree', (0, 255, 0)),
+    #('haarcascade_frontalface_alt'     , (0, 255, 0)),
+    #('haarcascade_frontalface_default' , (0, 255, 0)),
+    #('haarcascade_profileface'         , (0, 255, 0)),
+    ('lbpcascade_frontalface'          , (0, 255, 0)),
 
     # ===== Ear, mouth, nose =====
-    #('haarcascade_mcs_leftear.xml'         , path_to_haar, (127, 127, 255)),
-    #('haarcascade_mcs_rightear.xml'        , path_to_haar, (127, 127, 255)),
-    #('haarcascade_mcs_mouth.xml'           , path_to_haar, (127, 255, 255)),
-    #('haarcascade_mcs_nose.xml'            , path_to_haar, (255, 127, 127)),
+    #('haarcascade_mcs_leftear'         , (127, 255, 255)),
+    #('haarcascade_mcs_rightear'        , (127, 255, 255)),
+    #('haarcascade_mcs_mouth'           , (255, 127, 127)),
+    #('haarcascade_mcs_nose'            , (127, 127, 255)),
 
     # ===== Eye =====
-    #('haarcascade_eye_tree_eyeglasses.xml', path_to_haar, (0, 255, 0)),
-    ('haarcascade_eye.xml'                , path_to_haar, (0, 255, 223)),
-    #('haarcascade_lefteye_2splits.xml'    , path_to_haar, (0, 255, 0)),
-    #('haarcascade_righteye_2splits.xml'   , path_to_haar, (0, 255, 0)),
-    #('haarcascade_mcs_eyepair_big.xml'    , path_to_haar, (0, 255, 0)),
-    #('haarcascade_mcs_eyepair_small.xml'  , path_to_haar, (0, 255, 0)),
-    #('haarcascade_mcs_lefteye.xml'        , path_to_haar, (0, 255, 0)),
-    #('haarcascade_mcs_righteye.xml'       , path_to_haar, (0, 255, 0)),
+    #('haarcascade_eye_tree_eyeglasses', (0, 255, 223)),
+    ('haarcascade_eye'                , (0, 255, 223)),
+    #('haarcascade_lefteye_2splits'    , (0, 255, 223)),
+    #('haarcascade_righteye_2splits'   , (0, 255, 223)),
+    #('haarcascade_mcs_eyepair_big'    , (0, 255, 223)),
+    #('haarcascade_mcs_eyepair_small'  , (0, 255, 223)),
+    #('haarcascade_mcs_lefteye'        , (0, 255, 223)),
+    #('haarcascade_mcs_righteye'       , (0, 255, 223)),
 
     # ===== Body =====
-    #('haarcascade_fullbody.xml'           , path_to_haar, (0, 255, 0)),
-    #('haarcascade_lowerbody.xml'          , path_to_haar, (0, 255, 0)),
-    #('haarcascade_mcs_upperbody.xml'      , path_to_haar, (0, 255, 0)),
-    #('haarcascade_upperbody.xml'          , path_to_haar, (0, 255, 0)),
+    #('haarcascade_fullbody'           , (0, 255, 0)),
+    #('haarcascade_lowerbody'          , (0, 255, 0)),
+    #('haarcascade_mcs_upperbody'      , (0, 255, 0)),
+    #('haarcascade_upperbody'          , (0, 255, 0)),
 )
 
 # OpenCV cascade classifiers and the colors table.
 classifiers = list()
 colors = dict()
-for spec in specs:
-    full_path = os.path.join(spec[1], spec[0])
-    cfer = cv2.CascadeClassifier(full_path)
-    classifiers.append(cfer)
-    colors[cfer] = spec[2]
 
-# The end.
+# Process each classifier XML file in the configuration
+# by iterating specs and directories.
+for spec in specs:
+    for dir in dirs:
+
+        # Assemble the filename.
+        full_path = os.path.join(dir, spec[0] + '.xml')
+        
+        # Check validity of the XML file by creating
+        # OpenCV cascade classifier and testing its validity
+        # (skip if invalid.)
+        cfer = cv2.CascadeClassifier(full_path)
+        if cfer.empty():
+            continue
+
+        # Add to the module containers.
+        classifiers.append(cfer)
+        colors[cfer] = spec[1]
+
+if not classifiers:
+    print('*** Warning: No classifers configured. ***')
